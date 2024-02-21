4a3488d03121882f75b4fcd9917a82950170cada
Kayla Dale <kmeyers119@gmail.com>
2024-02-20 21:21:41 -0600

    initial commit

---
+++ Crud-API/src/test/java/edu/mcc/codeschool/Crud/API/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mcc.codeschool.Crud.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
